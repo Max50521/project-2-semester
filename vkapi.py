39c13c1ccd22423eca79729ddbf99c9a92df5f50
Max50521 <maxim_375@mail.ru>
2021-02-20 20:03:28 +0300

    group parsing 1

--- vkapi.py
+++ vkapi.py
@@ -3,17 +3,20 @@ import json
 import requests
 # my id 218576668  478809761
 user_id = 478809761
-a = "845407d3de9e28a28f7ba3e21689f40b92305eb6ebe297b6dd76839bcf38ff872ab78cc27f4a575da6210"
-r=requests.get("https://api.vk.com/method/friends.get", params={  #r=requests.get("https://api.vk.com/method/friends.get"
-    "user_id" : user_id,
-    "order": "name",
-    "count": 100,
-    "offset": 0,
-    "fields": "nickname, domain, sex, bdate, city, education, online, relation, last_seen",
-    "access_token" :a,
-    "v":5.122 #5.130
-}).json["response"]["items"]
+token = "845407d3de9e28a28f7ba3e21689f40b92305eb6ebe297b6dd76839bcf38ff872ab78cc27f4a575da6210"
+version = 5.92
+domain = "internetpasta"
 
-with open("friends.json",'w') as f:
-    f.write(json.dumps(r))
-f.close()
+response = requests.get('https://api.vk.com/method/wall.get',
+             params = {    'domain':domain,
+                           "access_token":token,
+                           'v':version,
+                      }
+                        )
+data = response.json()['response']['items']
+filename='data'
+myfile = open(filename, mode='w')
+json.dump(data, myfile)
+myfile.close()
+#myfile=open(data,mode='r')
+#vk_parsing=json.load(myfile)
