06e0444577dc5e2b1235bbbbbf7f88ae74793ce4
Max50521 <maxim_375@mail.ru>
2021-02-20 15:16:37 +0300

    not working

--- vkapi.py
+++ vkapi.py
@@ -1,6 +1,19 @@
 import vk
-access_token = "85f3dcef213096485f634ff73b99609a26205fc3f172eeb81a853ec6270a185e0a39c68b9db42eaa82d04"
-session = vk.Session(access_token=access_token)
-vk_api = vk.API(session)
-count=10
-vk_api.friends.get(users_id=478809761, v="5.130")
+import json
+import requests
+# my id 218576668  478809761
+user_id = 478809761
+a = "845407d3de9e28a28f7ba3e21689f40b92305eb6ebe297b6dd76839bcf38ff872ab78cc27f4a575da6210"
+r=requests.get("https://api.vk.com/method/friends.get", params={  #r=requests.get("https://api.vk.com/method/friends.get"
+    "user_id" : user_id,
+    "order": "name",
+    "count": 100,
+    "offset": 0,
+    "fields": "nickname, domain, sex, bdate, city, education, online, relation, last_seen",
+    "access_token" :a,
+    "v":5.122 #5.130
+}).json["response"]["items"]
+
+with open("friends.json",'w') as f:
+    f.write(json.dumps(r))
+f.close()
